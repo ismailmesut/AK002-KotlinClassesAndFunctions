13ca634208373e00e4af2f5984ebffd90a72bab1
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-03 10:41:06 +0300

    updated: functions - view details

--- app/src/main/java/com/ismailmesutmujde/kotlinclassesandfunctions/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinclassesandfunctions/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.ismailmesutmujde.kotlinclassesandfunctions
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var myTextView: TextView
+    lateinit var myButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myTextView = findViewById(R.id.textView)
+
+        /*
+        myButton.setOnClickListener {
+            myTextView.text = "button clicked 2"
+        }*/
 
         // 1) code block, reusable 2) input 3) output - return
 
@@ -30,16 +42,19 @@ class MainActivity : AppCompatActivity() {
 
     fun mySum(a:Int, b:Int) {
         //println(a+b)
-        //val myTextView = findViewById<TextView>(R.id.textView)
-        //myTextView.text = "Result : ${a+b}"
+
+
     }
 
     fun myMultiply(x:Int, y:Int) : Int {
         //println(x*y)
         val result = x * y
-        val myTextView = findViewById<TextView>(R.id.textView)
         myTextView.text = "Result : ${x*y}"
         return result
+    }
+
+    fun buttonClicked(view : View) {
+        myTextView.text = "Button Clicked"
     }
 
 }
