9de25f052da510f6cffa51b3b691ec151bb47fab
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-03 10:05:46 +0300

    updated: functions - get input

--- app/src/main/java/com/ismailmesutmujde/kotlinclassesandfunctions/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinclassesandfunctions/MainActivity.kt
@@ -10,13 +10,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 1) code block, reusable 2) input 3) output - return
+
         println("hello kotlin")
         test()
+        mySum(10,15)
+        mySum(40,35)
     }
 
     // function
     fun test() {
         println("test function")
+    }
+
+    fun mySum(a:Int, b:Int) {
+        println(a+b)
     }
 
 }
