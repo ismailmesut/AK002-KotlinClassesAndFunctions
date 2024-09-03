4dce01fcbf2ac12667870183e2809a7afebbe2f6
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-03 10:01:01 +0300

    updated: functions

--- app/src/main/java/com/ismailmesutmujde/kotlinclassesandfunctions/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinclassesandfunctions/MainActivity.kt
@@ -5,9 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        println("hello kotlin")
+        test()
     }
+
+    // function
+    fun test() {
+        println("test function")
+    }
+
 }
